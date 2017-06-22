58954b7acbad8629dc406ef0624af1f1ef2643b1
rbenites <benites.rod@gmail.com>
2017-06-22 16:11:36 -0300

    Correction of Tractography Visualization

--- js/tractography_3d.js
+++ js/tractography_3d.js
@@ -7,7 +7,7 @@ window.onload = function() {
     try {
         // try to create and initialize a 3D renderer
         threeD = new X.renderer3D();
-        threeD.container = '3d_tractography';
+        threeD.container = 'threeD_tractography_visual';
         threeD.init();
     } catch (Exception) {
 
@@ -21,9 +21,21 @@ window.onload = function() {
   	// .. associate the TrackVis .TRK file
   	fibers.file = 'volumes/dti.trk';
   	fibers.caption = 'The Corpus Callosum:<br>connecting the two hemispheres<br>of the human brain.';
-  
-  	// .. add the fibers
+//TODO Add gui to control opacity and line width	
+		fibers.linewidth = fibers.linewidth * 2;
+		fibers.opacity = 0.5;
+
+  	// Add the fibers
   	threeD.add(fibers);
+
+		//Create volume associated with fiber file
+		var volume = new X.volume();
+		volume.file = 'volumes/MICCAI2015_InVivo.nii';
+		volume.opacity = 0.01;		//Low opacity to be able to improve rotation
+		volume.volumeRendering = true;
+  
+		//Add The Volume
+		//threeD.add(volume);
 
     // start the loading/rendering
     //sliceX.render();
