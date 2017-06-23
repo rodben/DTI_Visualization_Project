bea82a65d60c78b42794b2fd0746aa7b9c3accb5
wilomaku <wjgarciah@unal.edu.co>
2017-06-23 20:29:24 -0300

    Modified file DWI

--- js/main_script.js
+++ js/main_script.js
@@ -11,10 +11,17 @@ window.onload = function() {
 
 		var multiplanar = new Multiplanar();
 		multiplanar.multiplanar_view();
+
+		var render_grad = new Grad_render();   
+    render_grad.load_grad_render();
+
+		//var main_volume_renderer_grad = new Main_volume_renderer_grad();   
+    //main_volume_renderer_grad.load_vol_render();
     
     document.getElementById("eigenvalue_01").oninput = function() { superquadric.eigenvalue01Handler("eigenvalue_01"); };
     document.getElementById("eigenvalue_02").oninput = function() { superquadric.eigenvalue02Handler("eigenvalue_02"); };
     document.getElementById("eigenvalue_03").oninput = function() { superquadric.eigenvalue03Handler("eigenvalue_03"); };
 
-	    
+    document.getElementById("gradients_sel").onchange = function() { render_grad.select_grad_render("gradients_sel"); };
+
 }
