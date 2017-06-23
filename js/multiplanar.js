1e3b5453a4c014e39d009f480477295e7cc4a975
rbenites <benites.rod@gmail.com>
2017-06-23 18:01:39 -0300

    Scalar index correction, colormap js refactory

--- js/multiplanar.js
+++ js/multiplanar.js
@@ -1,4 +1,6 @@
 var Multiplanar = function() {
+
+/********** FA Related ****************/
 		    _webGLFriendly = true;
     try {
         // try to create and initialize a 3D renderer
@@ -6,6 +8,11 @@ var Multiplanar = function() {
         this.r.container = 'scalar_index_multiplanar_fa';
 				this.r.bgColor = [.15, .15, .15];
         this.r.init();
+
+        this.render_md = new X.renderer3D();
+        this.render_md.container = 'scalar_index_multiplanar_md';
+				this.render_md.bgColor = [.15, .15, .15];
+        this.render_md.init();
     } catch (Exception) {
 
     // no webgl on this machine
@@ -20,90 +27,157 @@ var Multiplanar = function() {
 		this.fa_slice_y_index = this.fa_volume.indexY;
 		this.fa_slice_z_index = this.fa_volume.indexZ;
 
-		//Create Bounding Box
-		 this.box = new X.object();
-     this.box.points = new X.triplets(72);
-     this.box.normals = new X.triplets(72);
+		//Create Bounding Box for FA volume
+		 this.box_fa = new X.object();
+     this.box_fa.points = new X.triplets(72);
+     this.box_fa.normals = new X.triplets(72);
+
+/********** MD Related ****************/
+
+
+		this.md_volume = new X.volume();
+		this.md_volume.file = 'volumes/dti_MD.nii.gz';
+
+		this.md_slice_x_index = this.md_volume.indexX;
+		this.md_slice_y_index = this.md_volume.indexY;
+		this.md_slice_z_index = this.md_volume.indexZ;
+
+		//Create Bounding Box for FA volume
+		 this.box_md = new X.object();
+     this.box_md.points = new X.triplets(72);
+     this.box_md.normals = new X.triplets(72);
+
+
 
 }
 
 Multiplanar.prototype.multiplanar_view = function() {
 
+//----------------------------------------------------------------------------------------//
+/******************************************* FA Related **********************************/
+//---------------------------------------------------------------------------------------//		
 
 		//Add volume to render
 		this.r.add(this.fa_volume);
 
 		// callbacks
     var _this = this;
-
-		
 		
     // attach event!
     // the onShowtime method gets executed after all files were fully loaded and
     // just before the first rendering attempt
     this.r.onShowtime = function() {
-        // Hide Y and Z slices
-        //_this.fa_volume.children[1]['visible'] = false;
-        //_this.fa_volume.children[2]['visible'] = false;
 
         // CREATE Bounding Box
         var res = [_this.fa_volume.bbox[0],_this.fa_volume.bbox[2],_this.fa_volume.bbox[4]];
         var res2 = [_this.fa_volume.bbox[1],_this.fa_volume.bbox[3],_this.fa_volume.bbox[5]];
 
        
-        _this.box.type = 'LINES';
-        _this.box.points.add(res2[0], res[1], res2[2]);
-        _this.box.points.add(res[0], res[1], res2[2]);
-        _this.box.points.add(res2[0], res2[1], res2[2]);
-        _this.box.points.add(res[0], res2[1], res2[2]);
-        _this.box.points.add(res2[0], res[1], res[2]);
-        _this.box.points.add(res[0], res[1], res[2]);
-        _this.box.points.add(res2[0], res2[1], res[2]);
-        _this.box.points.add(res[0], res2[1], res[2]);
-        _this.box.points.add(res2[0], res[1], res2[2]);
-        _this.box.points.add(res2[0], res[1], res[2]);
-        _this.box.points.add(res[0], res[1], res2[2]);
-        _this.box.points.add(res[0], res[1], res[2]);
-        _this.box.points.add(res2[0], res2[1], res2[2]);
-        _this.box.points.add(res2[0], res2[1], res[2]);
-        _this.box.points.add(res[0], res2[1], res2[2]);
-        _this.box.points.add(res[0], res2[1], res[2]);
-        _this.box.points.add(res2[0], res2[1], res2[2]);
-        _this.box.points.add(res2[0], res[1], res2[2]);
-        _this.box.points.add(res[0], res2[1], res2[2]);
-        _this.box.points.add(res[0], res[1], res2[2]);
-        _this.box.points.add(res[0], res2[1], res[2]);
-        _this.box.points.add(res[0], res[1], res[2]);
-        _this.box.points.add(res2[0], res2[1], res[2]);
-        _this.box.points.add(res2[0], res[1], res[2]);
+        _this.box_fa.type = 'LINES';
+        _this.box_fa.points.add(res2[0], res[1], res2[2]);
+        _this.box_fa.points.add(res[0], res[1], res2[2]);
+        _this.box_fa.points.add(res2[0], res2[1], res2[2]);
+        _this.box_fa.points.add(res[0], res2[1], res2[2]);
+        _this.box_fa.points.add(res2[0], res[1], res[2]);
+        _this.box_fa.points.add(res[0], res[1], res[2]);
+        _this.box_fa.points.add(res2[0], res2[1], res[2]);
+        _this.box_fa.points.add(res[0], res2[1], res[2]);
+        _this.box_fa.points.add(res2[0], res[1], res2[2]);
+        _this.box_fa.points.add(res2[0], res[1], res[2]);
+        _this.box_fa.points.add(res[0], res[1], res2[2]);
+        _this.box_fa.points.add(res[0], res[1], res[2]);
+        _this.box_fa.points.add(res2[0], res2[1], res2[2]);
+        _this.box_fa.points.add(res2[0], res2[1], res[2]);
+        _this.box_fa.points.add(res[0], res2[1], res2[2]);
+        _this.box_fa.points.add(res[0], res2[1], res[2]);
+        _this.box_fa.points.add(res2[0], res2[1], res2[2]);
+        _this.box_fa.points.add(res2[0], res[1], res2[2]);
+        _this.box_fa.points.add(res[0], res2[1], res2[2]);
+        _this.box_fa.points.add(res[0], res[1], res2[2]);
+        _this.box_fa.points.add(res[0], res2[1], res[2]);
+        _this.box_fa.points.add(res[0], res[1], res[2]);
+        _this.box_fa.points.add(res2[0], res2[1], res[2]);
+        _this.box_fa.points.add(res2[0], res[1], res[2]);
         for ( var i = 0; i < 24; ++i) {
-            _this.box.normals.add(0, 0, 0);
+            _this.box_fa.normals.add(0, 0, 0);
         }
-        _this.r.add(_this.box);
+        _this.r.add(_this.box_fa);
 
         var center = [_this.fa_volume.bbox[0] + (_this.fa_volume.bbox[1]-_this.fa_volume.bbox[0]),
               _this.fa_volume.bbox[2] + (_this.fa_volume.bbox[3]-_this.fa_volume.bbox[2]),
               _this.fa_volume.bbox[4] + (_this.fa_volume.bbox[5]-_this.fa_volume.bbox[4])
               ]
-
-        // time to create the GUI!
-//        gui = new xslicegui(_this.r, _this.volume, _this.box);
-//        gui.create();
-
-    };
-
-//    // hide waiting screen after first render!
-//    this.r.onRender = function(){
-//        var loadingDiv = document.getElementById('loading');
-//        loadingDiv.style.display = 'none';
-//        
-//    }
+		}
 
     // adjust the camera position a little bit, just for visualization purposes
     this.r.camera.position = [270, 250, 330];
 
     this.r.render();
+
+//----------------------------------------------------------------------------------------//
+/******************************************* MD Related **********************************/
+//---------------------------------------------------------------------------------------//		
+
+		//Add volume to render
+		this.render_md.add(this.md_volume);
+
+		// callbacks
+    var _this = this;
+		
+    // attach event!
+    // the onShowtime method gets executed after all files were fully loaded and
+    // just before the first rendering attempt
+    this.render_md.onShowtime = function() {
+
+        // CREATE Bounding Box
+        var res = [_this.md_volume.bbox[0],_this.md_volume.bbox[2],_this.md_volume.bbox[4]];
+        var res2 = [_this.md_volume.bbox[1],_this.md_volume.bbox[3],_this.md_volume.bbox[5]];
+
+       
+        _this.box_md.type = 'LINES';
+        _this.box_md.points.add(res2[0], res[1], res2[2]);
+        _this.box_md.points.add(res[0], res[1], res2[2]);
+        _this.box_md.points.add(res2[0], res2[1], res2[2]);
+        _this.box_md.points.add(res[0], res2[1], res2[2]);
+        _this.box_md.points.add(res2[0], res[1], res[2]);
+        _this.box_md.points.add(res[0], res[1], res[2]);
+        _this.box_md.points.add(res2[0], res2[1], res[2]);
+        _this.box_md.points.add(res[0], res2[1], res[2]);
+        _this.box_md.points.add(res2[0], res[1], res2[2]);
+        _this.box_md.points.add(res2[0], res[1], res[2]);
+        _this.box_md.points.add(res[0], res[1], res2[2]);
+        _this.box_md.points.add(res[0], res[1], res[2]);
+        _this.box_md.points.add(res2[0], res2[1], res2[2]);
+        _this.box_md.points.add(res2[0], res2[1], res[2]);
+        _this.box_md.points.add(res[0], res2[1], res2[2]);
+        _this.box_md.points.add(res[0], res2[1], res[2]);
+        _this.box_md.points.add(res2[0], res2[1], res2[2]);
+        _this.box_md.points.add(res2[0], res[1], res2[2]);
+        _this.box_md.points.add(res[0], res2[1], res2[2]);
+        _this.box_md.points.add(res[0], res[1], res2[2]);
+        _this.box_md.points.add(res[0], res2[1], res[2]);
+        _this.box_md.points.add(res[0], res[1], res[2]);
+        _this.box_md.points.add(res2[0], res2[1], res[2]);
+        _this.box_md.points.add(res2[0], res[1], res[2]);
+        for ( var i = 0; i < 24; ++i) {
+            _this.box_md.normals.add(0, 0, 0);
+        }
+        _this.render_md.add(_this.box_md);
+
+        var center = [_this.md_volume.bbox[0] + (_this.md_volume.bbox[1]-_this.md_volume.bbox[0]),
+              _this.md_volume.bbox[2] + (_this.md_volume.bbox[3]-_this.md_volume.bbox[2]),
+              _this.md_volume.bbox[4] + (_this.md_volume.bbox[5]-_this.md_volume.bbox[4])
+              ]
+		}
+    // adjust the camera position a little bit, just for visualization purposes
+    this.render_md.camera.position = [270, 250, 330];
+
+    this.render_md.render();
+
 }
+
+
+//--------------------------  FA  ----------------------------------------------------//
 
 Multiplanar.prototype.FAsliceXHandler = function(x)
 {
@@ -136,4 +210,39 @@ Multiplanar.prototype.FAsliceZHandler = function(x)
 	//Render again	
 	this.r.render();
 
-}      
+}
+
+//--------------------------  MD  ----------------------------------------------------//
+
+Multiplanar.prototype.MDsliceXHandler = function(x)
+{
+	
+	//Update index with value
+	this.md_slice_x_index = parseFloat(document.getElementById(x).value);
+	this.md_volume.indexX = this.md_slice_x_index;
+	//Render again	
+	this.render_md.render();
+
+}
+
+Multiplanar.prototype.MDsliceYHandler = function(x)
+{
+	
+	//Update index with value
+	this.md_slice_y_index = parseFloat(document.getElementById(x).value);
+	this.md_volume.indexY = this.md_slice_y_index;
+	//Render again	
+	this.render_md.render();
+
+}  
+
+Multiplanar.prototype.MDsliceZHandler = function(x)
+{
+	
+	//Update index with value
+	this.md_slice_z_index = parseFloat(document.getElementById(x).value);
+	this.md_volume.indexZ = this.md_slice_z_index;
+	//Render again	
+	this.render_md.render();
+
+}            
