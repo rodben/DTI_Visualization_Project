d289bab194ab584886ca5c2918092684036c813c
rbenites <benites.rod@gmail.com>
2017-06-23 11:27:15 -0300

    Change structure of js script and reference volumes

--- js/main_script.js
+++ js/main_script.js
@@ -6,5 +6,8 @@ window.onload = function() {
     document.getElementById("eigenvalue_01").oninput = function() { superquadric.eigenvalue01Handler("eigenvalue_01"); };
     document.getElementById("eigenvalue_02").oninput = function() { superquadric.eigenvalue02Handler("eigenvalue_02"); };
     document.getElementById("eigenvalue_03").oninput = function() { superquadric.eigenvalue03Handler("eigenvalue_03"); };
+
+		multiplanar_view();
+		tractography_render_3d();
     
 }
